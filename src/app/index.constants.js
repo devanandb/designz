5624eff992cf4a92058cee359c9ad3a51e55e52a
Devanand <devanand.b@hp.com>
2015-12-05 20:15:02 +0530

    Adding first set of files

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('designz')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
